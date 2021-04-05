e60cb6ee91c648a173351893e1527f35ce7ee52d
Anderson Santana <anderslash@gmail.com>
2021-04-05 19:38:41 -0300

    Parte 2, ajustando dropdown

--- parte2/app.js
+++ parte2/app.js
@@ -1,4 +1,11 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
-
 navToggle.addEventListener('click', () => links.classList.toggle('show-links'));
+
+const institucional = document.querySelector('.red');
+const sublinks = document.querySelector('.sublinks');
+const arrow = document.querySelector('.arrow');
+institucional.addEventListener('click', () => {
+  sublinks.classList.toggle('show-links');
+  arrow.classList.toggle('fa-caret-up');
+});
