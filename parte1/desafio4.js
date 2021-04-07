28564558a00414b56c359174788b6eaa07519a9e
Anderson Santana <anderslash@gmail.com>
2021-04-07 02:00:17 -0300

    Parte 1, refatorando desafio 4

--- parte1/desafio4.js
+++ parte1/desafio4.js
@@ -1,12 +1,15 @@
 const body = document.querySelector('body');
-const modal = document.createElement('div');
-const overlay = document.createElement('div');
-const content = document.createElement('div');
-const img = document.createElement('img');
 
+const modal = document.createElement('div');
 Object.assign(modal, {className: 'modal micromodal-slide is-open'});
+
+const overlay = document.createElement('div');
 Object.assign(overlay, {className: 'modal__overlay'})
+
+const content = document.createElement('div');
 Object.assign(content, {className: 'position-relative'})
+
+const img = document.createElement('img');
 Object.assign(img, {
   className: 'form-modal',
   src: 'https://logodownload.org/wp-content/uploads/2015/05/palmeiras-logo-1.png'
